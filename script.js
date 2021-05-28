c2dde9aa9bccddb3c1b6d70b8ca570ff2487d33b
Akansha0211 <akansha.saxena0211@gmail.com>
2021-05-28 21:47:30 +0530

    Styling the column-name

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-let ans = "";
-let n = 1;
-while(n>0){
-    let rem = n%26;
-    if(rem == 0){
-        ans = "Z" + ans;
-        n = Math.floor(n/26)-1;
-    }else{
-        ans = String.fromCharCode(rem-1+65)+ ans;
-        n = Math.floor(n/26);
+$(document).ready(function(){
+    let cellContainer = $(".input-cell-container");
+    for(let i = 1; i<=100;i++){
+        let ans = "";
+        let n = i;
+        while(n>0){
+            let rem = n%26;
+            if(rem == 0){
+                ans = "Z" + ans;
+                n = Math.floor(n/26)-1;
+            }else{
+                ans = String.fromCharCode(rem-1+65)+ ans;
+                n = Math.floor(n/26);
+            }
+        }
+        let column = $(`<div class="column-name" id="colCod-${ans} col-id-${i}">${ans}</div>`);
+        $(".column-name-container").append(column);
+        
     }
-}
+    
+})
+
