6c3e7404489b1c3568189559d9dcb8e87524b006
Akansha0211 <akansha.saxena0211@gmail.com>
2021-05-28 22:43:27 +0530

    Add input cells to the input-cell-container

--- script.js
+++ script.js
@@ -13,11 +13,26 @@ $(document).ready(function(){
                 n = Math.floor(n/26);
             }
         }
-        let column = $(`<div class="column-name" id="colCod-${ans} col-id-${i}">${ans}</div>`);
+        let column = $(`<div class="column-name col-id-${i}" id="colCod-${ans} ">${ans}</div>`);
         $(".column-name-container").append(column);
         
         let row = $(`<div class="row-name" id=row-Id-${i}>${i}</div>`);
         $(".row-name-container").append(row);
+
+        
+        
+    }
+    //while traversing in row create a row, while traversing in col create a col
+    // and in the end append the row in the input-cell-container
+
+    for(let i = 1; i<=100; i++){
+        let row = $(`<div class="cell-row"></div>`);
+        for(let j = 1; j<=100; j++){
+            let colCode = $(`.col-id-${j}`).attr("id").split("-")[1];
+            let column = $(`<div class="input-cell " contenteditable="true" id="row-${i}-col-${j}" data="code-${colCode}"></div>`)
+            row.append(column);
+        }
+        $(".input-cell-container").append(row);
     }
     
 })
